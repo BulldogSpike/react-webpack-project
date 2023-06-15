49afda206deb2822cfbad9c979d6fa7034592213
Дмитрий Ерин <117025641+BulldogSpike@users.noreply.github.com>
2023-06-15 10:57:01 +0300

    logging info into console onSubmit

--- src/app.js
+++ src/app.js
@@ -1,98 +1,163 @@
-'use client'
-import CatImage from './cat4.jpg';
+"use client";
+import CatImage from "./cat4.jpg";
 // import Image from 'next/image'
 // import formImage from "../src/cat2.jpg"
-import { useFormik } from 'formik'
-import React from 'react';
-import './style.css'
+// import { useFormik } from "formik";
+import React, { useState } from "react";
+import "./style.css";
+// import { date } from "yup";
 // import InputForm from './inputform.js'
 
-
 export default function App() {
+  // const formik = useFormik({
+  //   initialValues: {
+  //     name: "",
+  //     facts: "",
+  //     url: "",
+  //     files: "",
+  //     date: "",
+  //   },
+  // });
 
-    const formik = useFormik({
-        initialValues: {
-          name: '',
-          facts: '',
-          url: '',
-          files: '',
-          date: ''
-        }
-      })
-    
-      console.log(formik.values);
-    
-      return (
-        <main className="h-screen flex items-center justify-center">
-          <form className='bg-white flex rounded-lg w-1/2 font-latoRegular' action="handler.php" method="post" encType='multipart/form-data'>
-            <div className='flex-1 text-gray-700 p-20'>
-              <h1 className='text-3xl pb-2 font-latoBold'>Тестовое задание в виде формы</h1>
-              <p className='text-lg text-gray-500'>
-                Very nice!
-              </p>
-              <div className='mt-6'>
-                {/* Name input field */}
-                <div className='pb-4'>
-                  <label className="block font-latoBold text-sm pb-2" htmlFor='name'>Ваше имя</label>
-                  <input className='border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500' 
-                  type="text" 
-                  name="name" 
-                  placeholder='Введите ваше имя' 
-                  value = {formik.values.name}
-                  onChange={formik.handleChange}
-                  required></input>
-                </div>
-                {/* Textarea input field */}
-                <div className='pb-4'>
-                  <label className="block font-latoBold text-sm pb-2" htmlFor='facts'>Расскажите о себе</label>
-                  <textarea className='border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500' 
-                  name="facts"
-                  value = {formik.values.facts}
-                  onChange={formik.handleChange} 
-                  placeholder='Факты о себе'></textarea>
-                </div>
-                {/* URL input field */}
-                <div className='pb-4'>
-                  <label className="block font-latoBold text-sm pb-2" htmlFor='url'>URL вашей компании</label>
-                  <input className='border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500' 
-                  type="url" 
-                  name="url" 
-                  value = {formik.values.url}
-                  onChange={formik.handleChange}
-                  placeholder='Введите URL'></input>
-                </div>
-                {/* Pictures input field */}
-                <div className='pb-4'>
-                  <label className="block font-latoBold text-sm pb-2" htmlFor='date'>Загрузите свои фотографии</label>
-                  <input className='border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500' 
-                  type="file" 
-                  name="pictures"
-                  value = {formik.values.files}
-                  onChange={formik.handleChange}
-                  placeholder='Прикрепите картинки'></input>
-                </div>
-                {/* Date input field */}
-                <div className='pb-4'>
-                  <label className="block font-latoBold text-sm pb-2" htmlFor='date'>Ваша дата рождения</label>
-                  <input className='border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500' 
-                  type="date" 
-                  name="date" 
-                  placeholder='Введите дату рождения'
-                  value = {formik.values.date}
-                  onChange={formik.handleChange}
-                  required></input>
-                </div>
-              </div>
-              <button type="submit" className='bg-teal-500 font-latoBold text-sm text-white py-3 mt-6 mr-8 rounded-lg w-1/3'>Отправить</button>
-              <button type="submit" className='bg-red-400 font-latoBold text-sm text-white py-3 mt-6 rounded-lg w-1/3'>Очистить</button>
+  const [name, setName] = useState("");
+  const [facts, setFacts] = useState("");
+  const [url, setUrl] = useState("");
+  const [files, setFiles] = useState([]);
+  const [date, setDate] = useState("");
+
+  function handleChange(e) {
+    setFiles([...e.target.files]);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(facts);
+    console.log(url);
+    console.log(files);
+    console.log(date);
+  };
+
+  // console.log(formik.values);
+
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <form
+        className="bg-white flex rounded-lg w-1/2 font-latoRegular"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex-1 text-gray-700 p-20">
+          <h1 className="text-3xl pb-2 font-latoBold">
+            Тестовое задание в виде формы
+          </h1>
+          <p className="text-lg text-gray-500">Very nice!</p>
+          <div className="mt-6">
+            {/* Name input field */}
+            <div className="pb-4">
+              <label
+                className="block font-latoBold text-sm pb-2"
+                htmlFor="name"
+              >
+                Ваше имя
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500"
+                type="text"
+                name="name"
+                placeholder="Введите ваше имя"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              ></input>
             </div>
-            <div className='relative flex-1'>
-              <img src={CatImage} alt="cat standing up"  className='object-cover object-right h-full w-full rounded-lg' />
+            {/* Textarea input field */}
+            <div className="pb-4">
+              <label
+                className="block font-latoBold text-sm pb-2"
+                htmlFor="facts"
+              >
+                Расскажите о себе
+              </label>
+              <textarea
+                className="border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500"
+                name="facts"
+                value={facts}
+                onChange={(e) => setFacts(e.target.value)}
+                placeholder="Факты о себе"
+              ></textarea>
             </div>
-          </form>
-        </main>
-      )
+            {/* URL input field */}
+            <div className="pb-4">
+              <label className="block font-latoBold text-sm pb-2" htmlFor="url">
+                URL вашей компании
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500"
+                type="url"
+                name="url"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                placeholder="Введите URL"
+              ></input>
+            </div>
+            {/* Pictures input field */}
+            <div className="pb-4">
+              <label
+                className="block font-latoBold text-sm pb-2"
+                htmlFor="date"
+              >
+                Загрузите свои фотографии
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500"
+                type="file"
+                name="pictures"
+                // value={files}
+                onChange={handleChange}
+                placeholder="Прикрепите картинки"
+                multiple
+              ></input>
+            </div>
+            {/* Date input field */}
+            <div className="pb-4">
+              <label
+                className="block font-latoBold text-sm pb-2"
+                htmlFor="date"
+              >
+                Ваша дата рождения
+              </label>
+              <input
+                className="border-2 border-gray-500 p-2 rounded-md w-3/4 focus:border-teal-500 focus:ring-teal-500"
+                type="date"
+                name="date"
+                placeholder="Введите дату рождения"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                required
+              ></input>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-teal-500 font-latoBold text-sm text-white py-3 mt-6 mr-8 rounded-lg w-1/3"
+          >
+            Отправить
+          </button>
+          <button
+            type="submit"
+            className="bg-red-400 font-latoBold text-sm text-white py-3 mt-6 rounded-lg w-1/3"
+          >
+            Очистить
+          </button>
+        </div>
+        <div className="relative flex-1">
+          <img
+            src={CatImage}
+            alt="cat standing up"
+            className="object-cover object-right h-full w-full rounded-lg"
+          />
+        </div>
+      </form>
+    </main>
+  );
 }
-
-
-
